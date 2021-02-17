c3bccdf5637cb3da9e94e6a2f7403468df883b43
Dloko <52128795+Dloko@users.noreply.github.com>
2021-02-17 19:16:11 -0300

    Update AppWeb_Directory_Collector.py

--- AppWeb_Directory_Collector.py
+++ AppWeb_Directory_Collector.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import readline
-import multiprocessing.dummy as mp
 
 apagar1 = os.popen("rm awdc.txt").read()
 apagar3 = os.popen("rm  awdc2.txt").read()
@@ -26,25 +25,25 @@ arquivo = open(awdc,"r").readlines()
 common = open("common.txt","r").readlines()
 Awdc = open("awdc2.txt","w")
 
+
 for linha in arquivo:
-    linha2 = linha.rstrip('\n')
-    d = "https://" + linha2 + "/"
-    print(d)
+    try:
+        linha2 = linha.rstrip('\n')
+        d = "https://" + linha2 + "/"
+        print(d)
 
-    for l in common:
-        f = l.rstrip('\n')
-        fd = d + f
-        r = requests.get(fd)
-        if r.status_code == 200:
-            Awdc.write(fd)
-            Awdc.write("\n")
+        for l in common:
+            f = l.rstrip('\n')
+            fd = d + f
 
+            r = requests.get(fd)
+            if r.status_code == 200:
+                Awdc.write(fd)
+                Awdc.write("\n")
 
-    def func1(*args):
         os.popen("dirb " + d + " -w common.txt -o awdc.txt").read()
-    p = mp.Pool(20)
-    p.map(func1, range(0, 1))
-    p.close()
+    except:
+        print(" ")
 
-    os.popen("cat awdc.txt | grep '" + d + "' | grep 'CODE:200' | cut -d ' ' -f2 | uniq -u >> awdc2.txt").read()
 
+os.popen("cat awdc.txt | grep '" + d + "' | grep 'CODE:200' | cut -d ' ' -f2 | uniq -u >> awdc2.txt").read()
